d20a4f18a6161ef902d8704c27b5c3630c3903d3
Mauricio Bedoya <bedoyadehezamauricio@gmail.com>
2023-07-19 20:07:09 -0400

    update redirectURI

--- src/helpers/spotify.js
+++ src/helpers/spotify.js
@@ -1,6 +1,6 @@
 const authEndpoint = "https://accounts.spotify.com/authorize?";
 const clientID = "0f07f63a507b42b29e58fc4c3def0253";
-const redirectURI = "http://localhost:5173/";
+const redirectURI = "https://spoty-player.vercel.app/";
 // access to user's library and playlists
 const scopes = ["user-library-read", "playlist-read-private"];
 
